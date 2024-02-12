a68faaf46573fd8aa7f897d459819a6219e7c00f
Moeshaney <nurlan.mhot@gmail.com>
2024-02-12 19:59:00 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function updateTimer() {
+    const now = new Date();
+    let nextFeb14 = new Date(now.getFullYear(), 1, 14, 19, 0, 0); // Установка цели на 19:00, 14 февраля текущего года
+
+    // Если текущая дата и время уже прошли 19:00 14 февраля, используем 14 февраля следующего года
+    if (now.getTime() > nextFeb14.getTime()) {
+        nextFeb14 = new Date(now.getFullYear() + 1, 1, 14, 19, 0, 0);
+    }
+
+    const diff = nextFeb14 - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / 1000 / 60) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
